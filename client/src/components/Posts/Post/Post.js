f57c144ba5c7e0dd546c3c22fb1f2767df0ca512
Xialu0803 <xialu.zou@sjsu.edu>
2021-11-11 14:23:40 -0800

    adding post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,10 +1,37 @@
 import React from "react";
 import useStyles from './styles';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from "@material-ui/core";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import moment from 'moment';
 
-const Post = () => {
+const Post = ({ post }) => {
     const classes = useStyles();
     return (
-        <h1>Post</h1>
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+            <div className={classes.overlay}>
+                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+            </div>
+            <div className={classes.overlay2}>
+                <Button style={{ color: "white" }} size="small" onclick={() => { }}>
+                    <MoreHorizIcon fontSize="default" />
+                </Button>
+            </div>
+            <div className={classes.details}>
+                <Typography variant="body2" color="textSecondary">{post.tags}</Typography>
+            </div>
+            <CardContent>
+                <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="red" onClick={() => { }}><FavoriteIcon fontSize="small" /> Like {post.likeCount} </Button> {/* dispatch(likePost(post._id)) */}
+                <Button size="small" color="primary" onClick={() => { }}><DeleteOutlineIcon fontSize="small" /> Delete</Button> {/* dispatch(deletePost(post._id)) */}
+            </CardActions>
+
+        </Card>
     );
 }
 
