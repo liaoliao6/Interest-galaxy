22a8cebb8b688ad918fa425f8efbb9350c172c9a
Xialu0803 <xialu.zou@sjsu.edu>
2021-11-10 21:27:05 -0800

    add two web pages

--- client/src/Personal.js
+++ client/src/Personal.js
@@ -1,28 +1,26 @@
 import React from "react";
 import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 //import Container from "@material-ui/lab/Container";
 //import AppBar from '@material-ui/lab/AppBar';
 //import Typography from "@material-ui/lab/Typography";
 import { Typography, Container, AppBar, Grow, Grid } from "@material-ui/core";
 import galaxy from "./images/galaxy.jpg";
-const App = () => {
+const Personal = () => {
     //const [currentId, setCurrentId] = useState(0);
     //const dispatch = useDispatch();
     //const classes = useStyles();
-
-
-
     return (
         <Container maxWidth="lg">
             <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center" >Interest Galaxy</Typography>
+                <Typography variant="h2" align="center" >Your Galaxy</Typography>
                 <img src={galaxy} alt="icon" height="80" />
             </AppBar>
             <Grow in>
                 <Container>
                     <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={12}>
-                            <Posts />
+                        <Grid item xs={12} sm={4}>
+                            <Form />
                         </Grid>
                     </Grid>
                 </Container>
@@ -33,4 +31,4 @@ const App = () => {
     );
 };
 
-export default App;
+export default Personal;
--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,7 @@ import react from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import Personal from "./Personal";
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Personal />, document.getElementById('root'));
