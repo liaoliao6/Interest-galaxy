3a58afda48699d70679258679be704f88055d333
Xialu0803 <xialu.zou@sjsu.edu>
2021-11-10 23:35:35 -0800

    change background of header

--- client/src/Personal.js
+++ client/src/Personal.js
@@ -1,20 +1,20 @@
 import React from "react";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
-//import Container from "@material-ui/lab/Container";
-//import AppBar from '@material-ui/lab/AppBar';
-//import Typography from "@material-ui/lab/Typography";
+
 import { Typography, Container, AppBar, Grow, Grid } from "@material-ui/core";
 import galaxy from "./images/galaxy.jpg";
+import useStyles from './styles';
+
 const Personal = () => {
     //const [currentId, setCurrentId] = useState(0);
     //const dispatch = useDispatch();
-    //const classes = useStyles();
+    const classes = useStyles();
     return (
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center" >Your Galaxy</Typography>
-                <img src={galaxy} alt="icon" height="80" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center" >Your Galaxy</Typography>
+                {/* <img className={classes.image} src={galaxy} alt="icon" height="80" /> */}
             </AppBar>
             <Grow in>
                 <Container>
---
+++ client/src/styles.js
@@ -0,0 +1,32 @@
+import { makeStyles } from "@material-ui/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import galaxy from "./images/galaxy.jpg";
+
+export default makeStyles(() => ({
+    appBar: {
+        borderRadius: 15,
+        margin: '30px 0',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundImage: `url(${galaxy})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+    },
+
+    heading: {
+        color: 'white',
+    },
+    /* image: {
+        marginLeft: '15px',
+    }, */
+}
+
+)
+
+
+
+
+);
