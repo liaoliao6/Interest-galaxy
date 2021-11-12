896f10c1fc8bfc8ecc75a3f3c085e830a25ab93f
Xialu0803 <xialu.zou@sjsu.edu>
2021-11-11 16:26:39 -0800

    add edit

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -28,7 +28,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 
         dispatch({ type: UPDATE, payload: data });
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
     }
 };
 
