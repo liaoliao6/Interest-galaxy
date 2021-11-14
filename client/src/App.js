a88332a4e199abfd9bbaf24264e5b76f70447a07
chelseyliu <msraxjlj@outlook.com>
2021-11-13 19:25:24 -0500

    Refactor and add auth nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
-import Posts from "./components/Posts/Posts";
-import { Typography, Container, AppBar, Grow, Grid } from "@material-ui/core";
-import galaxy from "./images/galaxy.jpg";
+import {Container,  Grow, Grid } from "@material-ui/core";
 import { useDispatch } from "react-redux";
-import { getPosts } from "./actions/posts"
-import useStyles from './styles';
+import { getPosts } from "./actions/posts";
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 const App = () => {
     const [currentId, setCurrentId] = useState(0);
     const dispatch = useDispatch();
-    const classes = useStyles();
     useEffect(() => {
         dispatch(getPosts());
     }, [dispatch]);
@@ -19,23 +19,15 @@ const App = () => {
      }, [currentId, dispatch]); */
 
     return (
-        <Container maxWidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center" >Interest Galaxy</Typography>
-                {/* <img className={classes.image} src={galaxy} alt="icon" height="80" /> */}
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={12}>
-                            <Posts />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-
-
-        </Container>
+        <BrowserRouter>
+            <Container maxWidth="lg">
+                <Navbar/>
+                <Switch>
+                    <Route path='/' exact component={Home}/>
+                    <Route path='/auth' exact component={Auth}/>
+                </Switch>
+            </Container>
+        </BrowserRouter>
     );
 };
 
